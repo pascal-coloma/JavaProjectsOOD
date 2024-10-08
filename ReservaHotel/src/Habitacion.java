c16ad9648e8329ad9d4cf1c7ab7460a5293d2108
pascal-coloma <pas.coloma@duocuc.cl>
2024-10-08 16:24:44 -0300

    Hotel reservation system created
    Beginning to work on a reservation system

---
+++ ReservaHotel/src/Habitacion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author colom
+ */
+public class Habitacion {
+    
+    private String numHab;
+    private double precioNoche;
+    private boolean disponible;
+    
+    
+        
+}
